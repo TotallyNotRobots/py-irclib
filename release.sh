9591b2df53114ec1bdd2fa74796a2b3528f42cc8
linuxdaemon <linuxdaemon.irc@gmail.com>
2025-10-25 05:48:28 +0000

    build: update build script to fix tags

--- release.sh
+++ release.sh
@@ -6,5 +6,5 @@ cz bump --changelog-to-stdout --git-output-to-stderr > "$changelog_file"
 
 git push origin main --tags
 
-gh release create "$(cz version -p)" -d -F "$changelog_file"
+gh release create "v$(cz version -p)" --verify-tag -t "Release v$(cz version -p)" -d -F "$changelog_file"
 rm "$changelog_file"
