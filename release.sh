aa16850392d25e44a8bef5091dc878ed884965cb
linuxdaemon <linuxdaemon.irc@gmail.com>
2026-05-08 23:59:48 +0000

    ci: make release pipeline consistent across TNR projects

--- release.sh
+++ release.sh
@@ -2,11 +2,11 @@
 set -euo pipefail
 
 changelog_file="$(mktemp)"
-cz bump --changelog-to-stdout --git-output-to-stderr > "$changelog_file"
+cz bump "${CZ_BUMP_ARGS:-}" --changelog-to-stdout --git-output-to-stderr > "$changelog_file"
 
 git push origin main
 sleep 1
 git push --tags
 sleep 1
-gh release create "v$(cz version -p)" --verify-tag -t "Release v$(cz version -p)" -F "$changelog_file"
+gh release create "v$(cz version -p)" --verify-tag -t "Release v$(cz version -p)" -d -F "$changelog_file"
 rm "$changelog_file"
